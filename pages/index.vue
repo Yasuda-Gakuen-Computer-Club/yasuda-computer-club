ca16263a03eaaf68712dc5b064f7d3c89fd4e240
Yasuda-gakuen-Computer-Club <github@yasuda-computer-club.org>
2018-10-02 18:44:49 +0900

    index.vue ヘッダー作成

--- pages/index.vue
+++ pages/index.vue
@@ -16,21 +16,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$max_width: 1200px;
+
 .header {
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 80px;
     border-bottom: 1px solid $theme_color;
 
     & .site-title {
-        display: flex;
-        align-items: center;
         width: 100%;
-        max-width: 1200px;
+        max-width: $max_width;
         height: 100%;
         margin: 0 auto;
+        display: flex;
+        align-items: center;
         color: $theme_color;
-        font-family: "GSanSerif-B";
-        font-weight: normal;
+        font: {
+            family: "GSanSerif-B";
+            weight: normal;
+            size: 36px;
+        }
+    }
+}
+.main {
+    width: 100%;
+    max-width: $max_width;
+    margin: 0 auto;
+    padding-top: 80px;
+    display: grid;
+    grid: {
+        template-columns: repeat(5, 1fr);
     }
 }
 </style>
