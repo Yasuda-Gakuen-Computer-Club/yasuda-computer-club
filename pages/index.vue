a825f34f3356c5605f31838d7083dd12bd3e452c
Fugaci <fugashi@yasuda-computer-club.org>
2018-10-03 18:43:40 +0900

    index.vue 小リンクのホバーアニメーション

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
         <main class="main">
             <div class="grid-container">
                 <div class="catch"></div>
-                <div class="grid-small"><nuxt-link to="about/">ABOUT</nuxt-link></div>
-                <div class="grid-small"><nuxt-link to="activities/">ACTIVITIES</nuxt-link></div>
+                <div class="grid-small link-about"><nuxt-link to="about/">ABOUT</nuxt-link></div>
+                <div class="grid-small link-activities"><nuxt-link to="activities/">ACTIVITIES</nuxt-link></div>
             </div>
         </main>
     </div>
@@ -24,6 +24,8 @@ export default {
 $grid_columns_pc: 8;
 $grid_size: 150px;
 $grid_small_size: 50px;
+$catch_span_row: 4;
+$catch_span_column: 5;
 $max_width: $grid_size * $grid_columns_pc;
 
 .header {
@@ -57,40 +59,75 @@ $max_width: $grid_size * $grid_columns_pc;
 
     & .grid-container {
         width: 100%;
+        padding-top: 5px;
         display: grid;
-        grid-template-rows: repeat(3, $grid_size) $grid_small_size;
+        grid-template-rows: repeat($catch_span_row - 2, $grid_size) repeat(2, $grid_small_size);
         grid-template-columns: repeat($grid_columns_pc, $grid_size);
         grid-auto-rows: $grid_size;
-
-        & > * {
-            margin: 2px;
-        }
+        grid-gap: 5px;
 
         & .catch {
-            grid-row: span 3;
-            grid-column: span 5;
+            grid-row: 1 / span $catch_span_row;
+            grid-column: 1 / span $catch_span_column;
             background-image: url("~assets/images/catch-image_1.jpg");
             background-size: cover;
             background-position: center center;
+            border-radius: 10px;
         }
 
         & .grid-small {
-            grid-row: 4;
-            grid-column: span $grid_columns_pc / 2;
+            position: relative;
+            grid-column: ($catch_span_column + 1) / span $grid_columns_pc - $catch_span_column;
+
+            &.link-about {
+                grid-row: 3;
+            }
+            &.link-activities {
+                grid-row: 4;
+            }
 
             & a {
                 width: 100%;
                 height: 100%;
+                position: relative;
                 display: flex;
                 justify-content: center;
                 align-items: center;
                 border: 1px solid $theme_color;
                 color: $theme_color;
                 letter-spacing: 5px;
-                transition: background-color .2s;
+                transition: letter-spacing .2s;
 
                 &:hover {
-                    background-color: rgba($theme_color, .05);
+                    letter-spacing: 7px;
+                }
+
+                &::before, &::after {
+                    content: "";
+                    display: block;
+                    position: absolute;
+                    border: 10px solid transparent;
+                    opacity: 0;
+                    transition: .2s;
+                }
+                &::before {
+                    top: 5px;
+                    left: 5px;
+                    border-top-color: $secondary_color;
+                    border-left-color: $secondary_color;
+                    transform: translate(-3px, -3px);
+                }
+                &::after {
+                    bottom: 5px;
+                    right: 5px;
+                    border-bottom-color: $secondary_color;
+                    border-right-color: $secondary_color;
+                    transform: translate(3px, 3px);
+                }
+
+                &:hover::before, &:hover::after {
+                    transform: none;
+                    opacity: 1;
                 }
             }
         }
