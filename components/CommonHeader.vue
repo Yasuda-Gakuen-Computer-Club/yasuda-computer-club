2c2c556ba5bb7d53c8d417d1f1cf80131787a376
Fugaci <fugashi@yasuda-computer-club.org>
2018-10-08 09:57:31 +0900

    aboutページ追加

---
+++ components/CommonHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <h1 class="site-title">
+            <nuxt-link
+                to="/"
+                class="link">
+                <img
+                    class="logo-image"
+                    src="~/assets/images/site-logo.svg"
+                    alt="安田学園 コンピュータークラブ">
+            </nuxt-link>
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "CommonHeader"
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    padding: 10px 20px;
+    border-bottom: 1px solid $theme_color;
+
+    & .site-title {
+        max-width: $max_width;
+        margin: 0 auto;
+
+        & .logo-image {
+            width: 100%;
+            max-width: 320px;
+            height: auto;
+        }
+    }
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <CommonHeader/>
+    </div>
+</template>
+
+<script>
+import CommonHeader from "~/components/CommonHeader.vue";
+
+export default {
+    name: "PageAbout",
+    components: {
+        CommonHeader
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
