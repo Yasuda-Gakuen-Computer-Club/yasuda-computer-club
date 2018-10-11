bfa6a23d203e9b199f4b7d1fb4cc149f24132567
Fugaci <fugashi@yasuda-computer-club.org>
2018-10-11 18:44:09 +0900

    favicon 追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,15 @@ module.exports = {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "東京都墨田区横網の安田学園コンピュータークラブです。" }
+            { hid: "description", name: "description", content: "東京都墨田区横網の安田学園コンピュータークラブです。" },
+            { name: "msapplication-TileColor", content: "#2A6DCB" }
         ],
         link: [
+            { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+            { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+            { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+            { rel: "manifest", href: "/site.webmanifest" },
+            { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#2A6DCB" },
             { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
         ]
     },
